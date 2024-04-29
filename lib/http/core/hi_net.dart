a238b3b0828667252b5acf44334543811271977c
beancurdv0@gmail.com <zhangfengcheng@shiguangjk.com>
2024-04-29 21:57:58 +0800

    mock 网络请求part two

---
+++ lib/http/core/hi_net.dart
@@ -0,0 +1,35 @@
+import 'package:demo001/http/request/base_request.dart';
+
+class HiNet {
+  HiNet._();
+
+  static late HiNet _instance;
+
+  static HiNet getInstance() {
+    _instance ??= HiNet._();
+    return _instance;
+  }
+
+  Future<dynamic> send<T>(BaseRequest request) async {
+    print('url:${request.url()}');
+    print('method:${request.httpMethod()}');
+    request.add('token', '123');
+    print('headers : ${request.header}');
+    // return Future(() => null)
+    return Future.value({
+      "statusCode": 200,
+      "data": {"code": 0, "message": "success"}
+    });
+  }
+
+  Future fire(BaseRequest request) async {
+    var response = await send(request);
+    var result = response['data'];
+    print(result);
+    return result;
+  }
+
+  void printLog(log:String) {
+    print('hi_net':${log.toString()});
+  }
+}
