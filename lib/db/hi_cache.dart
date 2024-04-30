058deac90550ac2efa5f6fb8b1e3c75e403cd995
beancurdv0@gmail.com <zhangfengcheng@shiguangjk.com>
2024-04-30 15:10:21 +0800

    [缓存] 北京

---
+++ lib/db/hi_cache.dart
@@ -0,0 +1,59 @@
+// 单例 + 预初始化
+import 'package:shared_preferences/shared_preferences.dart';
+
+class HiCache {
+  SharedPreferences? preferences;
+
+  HiCache._() {
+    init();
+  }
+
+  static HiCache? _instance;
+
+  static HiCache getInstance() {
+    _instance ??= HiCache._();
+    return _instance!;
+  }
+
+  void init() async {
+    if (preferences == null) {
+      preferences = await SharedPreferences.getInstance();
+    }
+  }
+
+  HiCache._pre(SharedPreferences prefs) {
+    preferences = prefs;
+  }
+
+  static Future<HiCache> preInit() async {
+    if (_instance == null) {
+      var prefs = await SharedPreferences.getInstance();
+      _instance = HiCache._pre(prefs);
+    }
+    return _instance!;
+  }
+
+  setBool(String key, bool value) {
+    preferences?.setBool(key, value);
+  }
+
+  setInt(String key, int value) {
+    preferences?.setInt(key, value);
+  }
+
+  setDouble(String key, double value) {
+    preferences?.setDouble(key, value);
+  }
+
+  setString(String key, String value) {
+    preferences?.setString(key, value);
+  }
+
+  setStringList(String key, List<String> value) {
+    preferences?.setStringList(key, value);
+  }
+
+  T get<T>(String key, T defaultValue) {
+    return preferences?.get(key) as T ?? defaultValue;
+  }
+}
